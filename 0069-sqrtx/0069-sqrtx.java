accc9f51204a6c2bd789964c306dc166ac6005e0
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-02-27 11:15:15 +0530

    Time: 1 ms (100.00%), Space: 40.3 MB (19.83%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,13 @@
 class Solution {
-public:
-    int mySqrt(int x) {
-        return sqrt(x);
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        int low = 1,high = x,ans =0;
+        while(low<=high){
+            int mid =low + (high-low)/2;
+            if(x/mid==mid)  return mid;
+            else if(x/mid<mid)   high=mid-1;
+            else {low = mid+1; ans = mid;}
+        }
+        return ans;
     }
-};
+}
