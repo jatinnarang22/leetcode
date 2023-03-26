0ca74f8b0a27d9c4524b2f1277ad7f1ca7829f29
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-03-26 12:33:08 +0530

    Time: 342 ms (12.50%), Space: 81 MB (75.00%) - LeetHub

---
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<long long>pre(n+1,0);
+        
+        for(int i=0;i<n;i++){
+            pre[i+1]=pre[i]+nums[i];
+        }
+        nums.insert(nums.begin(),0);
+        n++;
+        vector<long long>ans;
+        for(auto q : queries){
+            long long  l=lower_bound(nums.begin(),nums.end(),q)-nums.begin()-1;
+            long long u=upper_bound(nums.begin(),nums.end(),q)-nums.begin();
+            long long val=q*l-(pre[l]- pre[0]);
+            // cout<<val;
+            if(u!=n){
+                val+=(pre[n-1]-pre[u-1])-q*(n-u);
+            }
+            ans.push_back(val);
+        }
+        return ans;
+    }
+};
