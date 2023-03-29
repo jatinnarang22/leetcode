7694b37e978cb416967b43888f8cd95b5227068a
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-03-30 02:37:14 +0530

    Time: 19 ms (16.41%), Space: 42.5 MB (20.00%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -2,45 +2,48 @@ class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         
         Arrays.sort(satisfaction);
-        reverse(satisfaction);
+        // reverse(satisfaction);
+        // for(int i=0;i<satisfaction.length;i++)System.out.print(satisfaction[i]);
+        
         int n=satisfaction.length;
-        int ans=0;
-        int c=-1;
-        for(int i=n;i>0;i--){
-            int val=0;
-            int h=i;
-            c++;
-            for(int j=0;j<n-c;j++){
-                
-                val+=satisfaction[j]*h;
-                
-            // System.out.print(val);
-                h--;
+        int[][] dp =new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
             }
-            
-            System.out.print(val+" ");
-            ans=Math.max(ans,val);
         }
-        return ans;
+        return f(satisfaction,0,0,dp);
     }
-     public static void reverse(int[] array)
-    {
- 
-        // Length of the array
-        int n = array.length;
- 
-        // Swapping the first half elements with last half
-        // elements)
-        for (int i = 0; i < n / 2; i++) {
- 
-            // Storing the first half elements temporarily
-            int temp = array[i];
- 
-            // Assigning the first half to the last half
-            array[i] = array[n - i - 1];
- 
-            // Assigning the last half to the first half
-            array[n - i - 1] = temp;
+    public static int f(int[] satisfaction,int index,int temp,int[][] dp){
+        if(index == satisfaction.length){
+            return 0;
         }
+        if(dp[index][temp]!=-1){
+            return dp[index][temp];
+        }
+        // System.out.print(satisfaction[index]);
+        int taken =satisfaction[index]*(temp+1)+f(satisfaction,index+1,temp+1,dp);
+        int nottaken=0+f(satisfaction,index+1,temp,dp);
+        // System.out.print(taken);
+        return  dp[index][temp]=Math.max(taken,nottaken);
     }
 }
+//     public static void reverse(int[] array){
+// // Length of the array
+//         int n = array.length;
+ 
+//         // Swapping the first half elements with last half
+//         // elements)
+//         for (int i = 0; i < n / 2; i++) {
+ 
+//             // Storing the first half elements temporarily
+//             int temp = array[i];
+ 
+//             // Assigning the first half to the last half
+//             array[i] = array[n - i - 1];
+ 
+//             // Assigning the last half to the first half
+//             array[n - i - 1] = temp;
+//         }
+//     }
+// }
