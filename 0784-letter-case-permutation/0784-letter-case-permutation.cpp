a0603b02c29703880bc807b37d3f782f9cc70b3d
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-05-04 18:37:30 +0530

    Time: 14 ms (15.26%), Space: 11.3 MB (53.87%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    set<string>p;
-    void rec(string s, int i,string v,vector<string>&ans){
-        
-        if(i==s.size()){
-            cout<<v<<" ";
-            if(p.find(v)==p.end()){
-            p.insert(v);
-            ans.push_back(v);
-            v="";
-            }
+  void solve(string S, vector<string>&ans,int i) {
+        if(i == S.size()) { // base condition
+            ans.push_back(S);
             return;
         }
-        // cout<<i;
-        // rec(s,i+1,v+(s[i]),ans);
-            // cout<<toupper('a');
-            char h=tolower(s[i]);
-            rec(s,i+1,v+(h),ans);
-            char g=(toupper(s[i]));
-            rec(s,i+1,v+(g),ans);   
+        
+        if(isalpha(S[i])) { // if the scanned character is alphabet
+            S[i] = toupper(S[i]);
+            solve(S,ans,i+1);
+            S[i] = tolower(S[i]);
+            solve(S,ans,i+1);
+        }
+        else { // if the scanned character is number
+            solve(S,ans,i+1);
+        }
     }
-    vector<string> letterCasePermutation(string s) {
-        
-    vector<string>ans;
-        
-        rec(s,0,"",ans);
-        // set<string>ss;
-        // for(int i=0;i<ans.size();i++){
-        //     ss.insert(ans[i]);
-        // }
-        // vector<string>f;
-        // for(auto it:ss){
-        //     f.push_back(it);
-        // }
-        
+    vector<string> letterCasePermutation(string S) {
+        vector<string>ans;
+        solve(S,ans,0);
         return ans;
     }
+
 };
