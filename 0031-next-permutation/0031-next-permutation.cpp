5b784f6b3101fa5a2eadfb45872b28c9c6a04c72
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-05-08 19:16:32 +0530

    Time: 0 ms (100.00%), Space: 12 MB (93.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,31 +3,31 @@ public:
     void next_perm(int change,vector<int>& nums,int i){
         int flag=0;
         for(int j=i;j<nums.size();j++){
-            if(nums[j]<change){
-                swap(nums[j],nums[change]);
+            if(nums[j]<=nums[change]){
+                cout<<j<<" "<<change;
+                swap(nums[j-1],nums[change]);
                 flag=1;
                 break;
             }
         }
-        if(flag!=1){
+        if(flag==0){
             swap(nums[change],nums[nums.size()-1]);
         }
         sort(nums.begin()+i,nums.end());
         
     }
     void nextPermutation(vector<int>& nums) {
-        // int n=nums.size();
-        // int flag=0;
-        // for(int i=n-1;i>0;i--){
-        //     if(nums[i]>nums[i-1]){
-        //         next_perm(i-1,nums,i);
-        //         flag=1;
-        //         break;
-        //     }
-        // }
-        // if(flag==0){
-        //     sort(nums.begin(),nums.end());
-        // }
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        int flag=0;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                next_perm(i-1,nums,i);
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            sort(nums.begin(),nums.end());
+        }
     }
 };
