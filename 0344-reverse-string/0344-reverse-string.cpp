72a347ae51d1528558df36f9dfdf880549e2f802
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-06-30 07:24:43 -0700

    Time: 29 ms (26.80%), Space: 23.1 MB (94.59%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        string helper;
-        for(int i=0;i<s.size();i++){
-            helper+=s[i];
-        }
-        int j=0;
-        for(int i=helper.size()-1;i>=0;i--){
-            // cout<<helper[0];
-            s[j]=helper[i];
-            j++;
+        for(int i=0;i<s.size()/2;i++){
+            char c=s[i];
+            s[i]=s[s.size()-i-1];
+            s[s.size()-i-1]=c;
+            
         }
     }
 };
