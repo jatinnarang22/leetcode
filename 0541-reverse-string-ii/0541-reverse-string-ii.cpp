010421abd80c31a9f0cfa7271cd57df2ec7739ca
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-06-30 08:30:20 -0700

    Time: 0 ms (100.00%), Space: 8.3 MB (8.77%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -1,14 +1,30 @@
 class Solution {
-    public String reverseStr(String s, int k) {
-                char[] a = s.toCharArray();
-        for (int start = 0; start < a.length; start += 2 * k) {
-            int i = start, j = Math.min(start + k - 1, a.length - 1);
-            while (i < j) {
-                char tmp = a[i];
-                a[i++] = a[j];
-                a[j--] = tmp;
+public:
+    string reverseStr(string s, int k) {
+     
+        // if(s.size()<k){
+        //     return reverse(s.begin(),s.end());
+        // }
+        int n=s.size();
+          if(k>n)
+        { 
+            reverse(s.begin(),s.end());
+            return s;
+        }
+        int j=0;
+        string ans;
+        for(int i=0;i<s.size();i+=2*k){
+            string temp=s.substr(i,k);
+            // cout<<temp;
+            reverse(temp.begin(),temp.end());
+            cout<<temp<< " ";
+            ans+=temp;
+            j+=k;
+            while(j < n && j<i+2*k){
+                ans.push_back(s[j]);
+                j++;
             }
         }
-        return new String(a);
+        return ans;
     }
-}
+};
