52ea8e092522d1f14f9cf429e3497b5576b14ba6
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-07-02 04:48:44 -0700

    Time: 15 ms (20.81%), Space: 11.4 MB (53.37%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
- bool static compare(pair<int, int>& a, pair<int, int>& b){
-   if(a.second == b.second) {
-        return a.first > b.first;
-    }
-    return a.second<b.second;
-};
-vector<int>frequencySort(vector<int>nums){
-    vector<pair<int,int>>freq;
-    int cnt=1;
-    for(int i=0;i<nums.size();i++){
-         bool found = false;
-        for(auto& p : freq) {
-            if(p.first == nums[i]) {
-                p.second++;
-                found = true;
-                break;
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int>m;
+        
+        for(auto it:nums){
+            m[it]++;
+        }
+        priority_queue<pair<int,int>>q;
+        for(auto it: m){
+            q.push({-it.second,it.first});
+        }
+        
+        vector<int>result;
+        
+        while(!q.empty()){
+            int val=q.top().first;
+            int num=q.top().second;
+        q.pop();
+            
+            while(val<0){
+                result.push_back(num);
+                val++;
             }
         }
-        if(!found) {
-            freq.push_back({nums[i], 1});
-        }
+        return result;
+        
     }
-    sort(freq.begin(), freq.end(), compare);
-     vector<int> result;
-    for(auto& p : freq) {
-        for(int i = 0; i < p.second; i++) {
-            result.push_back(p.first);
-        }
-    }
-    
-    return result;
-    
-    
-}
 };
