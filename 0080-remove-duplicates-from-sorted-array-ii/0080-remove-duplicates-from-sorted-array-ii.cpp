17024be88a1d72025d6969900423f7decf879348
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-07-20 11:11:02 -0700

    Time: 4 ms (86.16%), Space: 10.8 MB (89.07%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int i=0;
+        for(auto e:nums){
+            if(i==0 || i==1 || nums[i-2]!=e){
+                nums[i]=e;
+                i+=1;
+            }
+            else{
+                cout<<e;
+            }
+        }
+        return i;
+    }
+};
