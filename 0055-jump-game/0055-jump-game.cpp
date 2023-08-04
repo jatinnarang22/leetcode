692832a6450b9a34e1d288dd0430b6605d758d71
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-08-04 09:46:00 +0530

    Time: 128 ms (23.91%), Space: 48.5 MB (40.20%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    bool fun(vector<int>& nums, int n, int i, vector<int>& dp) {
-        if (i == n - 1) {
-            return true;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        if (nums[i] == 0 || i >= n) {
-            return false;
-        }
-        for (int j = 1; i + j < n && j <= nums[i]; j++) {
-            if (fun(nums, n, i + j, dp)) {
-                return dp[i] = true;
-            }
-        }
-        return dp[i] = false;
-    }
-    
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return fun(nums, n, 0, dp);
+        int mx=0;
+        int flag=0;
+        for(int i=0;i<nums.size();i++){
+            cout<<mx;
+            if(mx<i ){
+                flag=1;
+                return false;
+            }
+            mx=max(mx,i+nums[i]);
+            
+        }
+        if(flag==0 )return true;
+        return false;
     }
 };
