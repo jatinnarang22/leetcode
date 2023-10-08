3fbac4d740b90a972083e369e5f1520ab0b1fd29
jatin narang <78208475+jatinnarang22@users.noreply.github.com>
2023-10-08 13:55:38 +0530

    Time: 172 ms (50.00%), Space: 98.1 MB (66.67%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.cpp
+++ 2895-minimum-processing-time/2895-minimum-processing-time.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) {
-        sort(processorTime.begin(),processorTime.end(),greater());
-        sort(tasks.begin(),tasks.end());
-        int val=1;
+        sort(processorTime.begin(),processorTime.end());
+        sort(tasks.begin(),tasks.end(),greater());
+        int val=0;
         int j=0;
         int ans=INT_MIN;
-        for(int i=0;i<tasks.size();i++){
-            if(val>4){
-                j++;
-                val=1;
-            }
+        for(int i=0;i<tasks.size();){
+            
             cout<<processorTime[j]+tasks[i]<<" ";
             ans=max(ans,processorTime[j]+tasks[i]);
-            val++;
+            i+=4;
+            j++;
             
         }
         
